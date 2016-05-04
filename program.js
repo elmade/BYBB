c3b4b67d9aa4bde4ed756108247ac79eb21197ec
elmade <elena.mahal@gmx.de>
2016-05-04 20:54:19 +0200

    Tabellenzellen mit der Größe 50x50 Pixel
    Zellengröße festgelegt

--- program.js
+++ program.js
@@ -7,23 +7,28 @@
 
 //}
 
-tab = "<table>"; //hier wird die tabelle göffnet
+tabelle = "<table>"; //hier beginnt die Tabelle für das Labyrinth
 
-for(i=0;i<5;i++) 
+for(i=0;i<5;i++) //Zahl hinter i< bzw. ii< zeigt an, wie viele Zellen es geben soll
 { 
-tab = tab + "<tr>"; 
+tabelle = tabelle + "<tr>"; 
 
 for(ii=0;ii<5;ii++){ 
 id = i*5 + ii; 
-tab = tab + "<td id=\""+id+"\">" + id + "</td>"; 
+tabelle = tabelle + "<td width = 50px height = 50px id=\""+id+"\">" + id + "</td>"; 
 } 
-tab = tab + "</tr>"; 
+tabelle = tabelle + "</tr>"; 
 }
 
-tab = tab + "</table>"; 
-document.write(tab);
+tabelle = tabelle + "</table>"; 
+document.write(tabelle);
+
+//Festlegung der Mauer
+
 
 document.getElementById("0").style.backgroundColor = 'brown';
+
+
 document.getElementById("1").style.backgroundColor = 'brown';
 document.getElementById("8").style.backgroundColor = 'brown';
 document.getElementById("10").style.backgroundColor = 'brown';
