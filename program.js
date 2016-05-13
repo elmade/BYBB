644e2757d8813deb0c57d7a93910fc09908a9e46
Sarflo <ernstsarah@gmx.de>
2016-05-13 16:29:12 +0200

    Neuer Versuch mit Canvas
    Beispiel aus dem Internet mit Canvas. Inkl Sprite und Steuerung.
    Funktioniert noch nicht...

--- program.js
+++ program.js
@@ -31,114 +31,7 @@ document.getElementById("22").style.backgroundColor = 'brown';
 document.getElementById("23").style.backgroundColor = 'brown';
 document.getElementById("24").style.backgroundColor = 'brown';*/
 
-var canvas;
-var ctx;
-var dx = 5;
-var dy = 5;
-var x = 200;
-var y = 5;
-var WIDTH = 482;
-var HEIGHT = 482;
-var img = new Image();
-var collision = 0;
 
-function rect(x,y,w,h) {
-ctx.beginPath();
-ctx.rect(x,y,w,h);
-ctx.closePath();
-ctx.fill();
-}
-
-function clear() {
-ctx.clearRect(0, 0, WIDTH, HEIGHT);
-ctx.drawImage(img, 0, 0);
-}
-
-function init() {
-	canvas = document.getElementById("cancas");
-ctx = canvas.getContext("2d");
-img.src = "maze.gif";
-return setInterval(draw, 10);
-}
-
-function doKeyDown(evt){
-switch (evt.keyCode) {
-case 38:  /* Up arrow was pressed */
-if (y - dy > 0){
-y -= dy;
-clear();
-checkcollision();
-if (collision == 1){
-y += dy;
-collision = 0;
-}
-}
-
-break;
-case 40:  /* Down arrow was pressed */
-if (y + dy < HEIGHT ){
-y += dy;
-clear();
-checkcollision();
-if (collision == 1){
-y -= dy;
-collision = 0;
-}
-}
-
-break;
-case 37:  /* Left arrow was pressed */
-if (x - dx > 0){
-x -= dx;
-clear();
-checkcollision();
-if (collision == 1){
-x += dx;
-collision = 0;
-}
-}
-break;
-case 39:  /* Right arrow was pressed */
-if ((x + dx < WIDTH)){
-x += dx;
-clear();
-checkcollision();
-if (collision == 1){
-x -= dx;
-collision = 0;
-}
-}
-break;
-}
-}
-
-function checkcollision() {
-var imgd = ctx.getImageData(x, y, 15, 15);
-var pix = imgd.data;
-for (var i = 0; n = pix.length, i < n; i += 4) {
-if (pix[i] == 0) {
-collision = 1;
-}
-}
-}
-
-function draw() {
-clear();
-ctx.fillStyle = "purple";
-rect(x, y, 15,15);
-}
-
-init();
-window.addEventListener('keydown',doKeyDown,true);
-</script>
-</section>
-<aside> </aside>
-<footer> </footer>
-</body>
-</html>
-
-<<<<<<< HEAD
-var bgImage = tabelle;
 
 //Knopf für Bewegung nach rechts
 //funktioniert noch nicht
@@ -146,5 +39,78 @@ var bgImage = tabelle;
 //function myFunction(move 100) {
 //document.getElementById("mytitle").innerHTML = "Dieser Text wurde vom Skript geschrieben";
 //}
-=======
->>>>>>> origin/master
+
+var canvas = $('#GameBoardCanvas');
+//The game board 1 = walls, 0 = free space, and -1 = the goal
+var board = [
+    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+    [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
+    [ 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
+    [ 0, 1, 1, 0, 0, 0, 1, 0, 1, 0],
+    [ 0, 0, 1, 1, 1, 1, 1, 0, 1, 0],
+    [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
+    [ 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
+    [ 1, 0, 1, 0, 1, 0, 0, 1, 1, 0],
+    [-1, 0, 1, 0, 1, 1, 0, 0, 0, 0]
+];
+var player = {
+    x: 0,
+    y: 0
+};
+
+//Draw the game board
+function draw(){
+    var width = canvas.width();
+    var blockSize = width/board.length;
+    var ctx = canvas[0].getContext('2d');
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, width, width);
+    ctx.fillStyle="white";
+    //Loop through the board array drawing the walls and the goal
+    for(var y = 0; y < board.length; y++){
+        for(var x = 0; x < board[y].length; x++){
+            //Draw a wall
+            if(board[y][x] === 1){
+                ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
+            }
+            //Draw the goal
+            else if(board[y][x] === -1){
+                ctx.beginPath();
+                ctx.lineWidth = 5;
+                ctx.strokeStyle = "gold";
+                ctx.moveTo(x*blockSize, y*blockSize);
+                ctx.lineTo((x+1)*blockSize, (y+1)*blockSize);
+                ctx.moveTo(x*blockSize, (y+1)*blockSize);
+                ctx.lineTo((x+1)*blockSize, y*blockSize);
+                ctx.stroke();
+            }
+        }
+    }
+    //Draw the player
+    ctx.beginPath();
+    var half = blockSize/2;
+    ctx.fillStyle = "blue";
+    ctx.arc(player.x*blockSize+half, player.y*blockSize+half, half, 0, 2*Math.PI);
+    ctx.fill();
+}
+
+//Check to see if the new space is inside the board and not a wall
+function canMove(x, y){
+    return (y>=0) && (y<board.length) && (x >= 0) && (x < board[y].length) && (board[y][x] != 1);
+}
+
+$(document).keyup(function(e){
+    if((e.which == 38) && canMove(player.x, player.y-1))//Up arrow
+        player.y--;
+    else if((e.which == 40) && canMove(player.x, player.y+1)) // down arrow
+        player.y++;
+    else if((e.which == 37) && canMove(player.x-1, player.y))
+        player.x--;
+    else if((e.which == 39) && canMove(player.x+1, player.y))
+        player.x++;
+    draw();
+    e.preventDefault();
+});
+
+draw();
