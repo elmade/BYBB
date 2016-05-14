849e0c70138c1b67c326137d5d0d12d3fd352c72
Sarflo <ernstsarah@gmx.de>
2016-05-14 13:09:07 +0200

    Beispiel von anderer Gruppe ausprobiert
    funktioniert auch nicht...
    Button eingefügt, mit dem man das Spiel starten kann.

--- program.js
+++ program.js
@@ -40,77 +40,108 @@ document.getElementById("24").style.backgroundColor = 'brown';*/
 //document.getElementById("mytitle").innerHTML = "Dieser Text wurde vom Skript geschrieben";
 //}
 
-var canvas = $('#GameBoardCanvas');
-//The game board 1 = walls, 0 = free space, and -1 = the goal
-var board = [
-    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-    [ 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
-    [ 0, 1, 1, 0, 0, 0, 1, 0, 1, 0],
-    [ 0, 0, 1, 1, 1, 1, 1, 0, 1, 0],
-    [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
-    [ 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
-    [ 1, 0, 1, 0, 1, 0, 0, 1, 1, 0],
-    [-1, 0, 1, 0, 1, 1, 0, 0, 0, 0]
-];
-var player = {
-    x: 0,
-    y: 0
+
+      function Spiel(){
+	  
+	  var canvas, 
+        context,
+        bounce = -1;
+        
+    
+    canvas = $("#canvas")[0];
+    context = canvas.getContext("2d");
+    var w = canvas.width;
+    var h = canvas.height; 
+//d:delimiter
+var delimiter = 10;
+var size = w/delimiter;
+
+
+for(var i = 0;i<10;i++){
+        for(var j = 0;j<10;j++){       
+            rect(size*i,size*j,size,size,randomcolor());
+
+    };
 };
 
-//Draw the game board
-function draw(){
-    var width = canvas.width();
-    var blockSize = width/board.length;
-    var ctx = canvas[0].getContext('2d');
-    ctx.setTransform(1, 0, 0, 1, 0, 0);
-    ctx.clearRect(0, 0, width, width);
-    ctx.fillStyle="white";
-    //Loop through the board array drawing the walls and the goal
-    for(var y = 0; y < board.length; y++){
-        for(var x = 0; x < board[y].length; x++){
-            //Draw a wall
-            if(board[y][x] === 1){
-                ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
-            }
-            //Draw the goal
-            else if(board[y][x] === -1){
-                ctx.beginPath();
-                ctx.lineWidth = 5;
-                ctx.strokeStyle = "gold";
-                ctx.moveTo(x*blockSize, y*blockSize);
-                ctx.lineTo((x+1)*blockSize, (y+1)*blockSize);
-                ctx.moveTo(x*blockSize, (y+1)*blockSize);
-                ctx.lineTo((x+1)*blockSize, y*blockSize);
-                ctx.stroke();
-            }
-        }
-    }
-    //Draw the player
-    ctx.beginPath();
-    var half = blockSize/2;
-    ctx.fillStyle = "blue";
-    ctx.arc(player.x*blockSize+half, player.y*blockSize+half, half, 0, 2*Math.PI);
-    ctx.fill();
-}
 
-//Check to see if the new space is inside the board and not a wall
-function canMove(x, y){
-    return (y>=0) && (y<board.length) && (x >= 0) && (x < board[y].length) && (board[y][x] != 1);
-}
 
-$(document).keyup(function(e){
-    if((e.which == 38) && canMove(player.x, player.y-1))//Up arrow
-        player.y--;
-    else if((e.which == 40) && canMove(player.x, player.y+1)) // down arrow
-        player.y++;
-    else if((e.which == 37) && canMove(player.x-1, player.y))
-        player.x--;
-    else if((e.which == 39) && canMove(player.x+1, player.y))
-        player.x++;
-    draw();
-    e.preventDefault();
+
+ var Maze = new Array(
+                      new Array(1,1,1,1,1,1,1,1,1,1),
+                      new Array(1,0,0,0,1,1,1,0,0,0),
+                      new Array(1,1,1,0,0,0,0,0,1,1),
+                      new Array(1,0,0,0,1,1,1,0,1,1),
+                      new Array(1,0,1,1,1,1,0,1,1,1),
+                      new Array(1,0,0,1,1,1,0,0,0,1),
+                      new Array(1,1,0,0,0,1,0,1,0,1),
+                      new Array(1,1,1,1,0,1,0,1,0,1),
+                      new Array(1,0,0,0,0,0,0,1,0,1),
+                      new Array(1,0,1,1,1,1,1,1,1,1)
+                      ); 
+
+
+function BlackOrWhite(something){
+    if(something == 1){return "black";}else{return "white";};
+};
+
+
+            
+
+for(var i = 0;i<10;i++){
+        for(var j = 0;j<10;j++){       
+            rect(size*j,size*i,size,size,BlackOrWhite((Maze[i][j])));
+
+    };
+};
+
+
+
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++
+//     CANVVAS UTILITIEZ
+//+++++++++++++++++++++++++++++++++++++++++
+
+function rect(x,y,w,h,color){
+    context.fillStyle = color;  
+    context.fillRect (x,y,w,h);  
+};
+
+function randomcolor(){
+    var r=Math.floor(Math.random()*255),
+        g=Math.floor(Math.random()*255),
+        b=Math.floor(Math.random()*255),
+        a=Math.random(),
+        rgba='rgba('+r+','+g+','+b+','+a+')';
+    return rgba;
+
+};
+
+function rnd(num){
+    return Math.random()*num;
+};
+
+ $("#canvas").dblclick(function(){
+    context.clearRect(0, 0, 800, 800);    
 });
 
-draw();
+function Line(x1,y1,x2,y2){
+    //context.lineWidth = 0.1;
+    context.beginPath();
+    context.moveTo(x1,y1);
+    context.lineTo(x2,y2);
+    context.stroke();
+};
+
+
+function Circle(x,y,radius,startAngle,endAngle,clockwise){
+
+    context.beginPath();
+    context.arc(x,y,radius,startAngle,endAngle,clockwise);
+    context.closePath();
+    context.fill();     
+};
+
+	  };
