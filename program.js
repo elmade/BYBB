785744b055fb2a1197bba4af9046d963a76b8680
elmade <elena.mahal@gmx.de>
2016-05-25 12:38:46 +0200

    Kommentare hinzugefügt, Layout geändert
    Kommentar zur Erläuterung eingefügt, neues Labyrinth, Farben geändert

--- program.js
+++ program.js
@@ -1,26 +1,27 @@
 
 function Bybb(){
-	var canvas;
-	var board;
-	var player;
+	var canvas; //Variable für Spielfeld
+	var board; //Variable für Labyrinth
+	var player; //Variable für Spieler
+
 	
-canvas = document.getElementById("GameBoardCanvas");
+canvas = document.getElementById("GameBoardCanvas"); //holt Angaben zu Canvas aus HTML-Datei
 
 
-//var canvas = $('#GameBoardCanvas');
 
-//The game board 1 = walls, 0 = free space, and -1 = the goal
+//Labyrinth festlegen: m = Mauer, w = Weg, z = Ziel, b = Buch
 board = [
-    [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [ 1, 1, 1, 1, 1, 1, 1, 1, 1, 0],
-    [ 1, 0, 1, 0, 0, 0, 0, 0, 1, 0],
-    [ 0, 0, 0, 0, 1, 1, 1, 0, 1, 0],
-    [ 0, 1, 1, 0, 0, 0, 1, 0, 1, 0],
-    [ 0, 0, 1, 1, 1, 1, 1, 0, 1, 0],
-    [ 1, 0, 1, 0, 0, 0, 1, 0, 1, 0],
-    [ 1, 0, 1, 0, 1, 0, 1, 0, 0, 0],
-    [ 1, 0, 1, 0, 1, 0, 0, 1, 1, 0],
-    [-1, 0, 1, 0, 1, 1, 0, 0, 0, 0]
+
+	[ "w", "w", "w", "w", "m", "w", "m", "m", "m", "w"],
+    [ "m", "m", "m", "w", "m", "w", "w", "w", "w", "w"],
+    [ "w", "w", "w", "w", "m", "m", "w", "m", "m", "w"],
+    [ "m", "m", "w", "m", "m", "w", "w", "w", "m", "w"],
+    [ "w", "m", "w", "m", "w", "w", "m", "w", "m", "w"],
+    [ "w", "m", "w", "m", "b", "m", "m", "w", "m", "w"],
+    [ "w", "m", "w", "m", "m", "m", "w", "w", "m", "w"],
+    [ "w", "w", "w", "w", "w", "w", "w", "m", "m", "w"],
+    [ "m", "m", "m", "w", "m", "m", "m", "m", "m", "w"],
+    [ "m", "w", "w", "w", "w", "w", "w", "w", "m", "z"]
 ];
 
 //Startposition des Spielers
@@ -32,21 +33,24 @@ player = {
 
 //Labyrinth zeichnen
 function draw(){
-    var width = canvas.width;
-    var blockSize = width/board[0].length;
+    var width = canvas.width; //Labyrinth passt sich der Breite des Canvas an
+    var blockSize = width/board[0].length; //Blockgröße ist abhängig von board
     var ctx = canvas.getContext('2d');
-    //ctx.setTransform(1, 0, 0, 1, 0, 0);
+    //ctx.setTransform(1, 0, 0, 1, 0, 0); //wird scheinbar nicht gebraucht
     ctx.clearRect(0, 0, width, width);
-    ctx.fillStyle="white";
+    ctx.fillStyle="maroon"; //Farbe für Mauer
+	
     //Schleife durch Array für das Zeichnen der Wände und des Ziels
-    for(var y = 0; y < board.length; y++){
+    for(var y = 0; y < board.length; y++){ //Schleife beginnt links oben im Spielfeld, läuft durch Array, Endposition ist unten rechts im Spielfeld
         for(var x = 0; x < board[y].length; x++){
-            //Draw a wall
-            if(board[y][x] === 1){
+            //Mauer wird gezeichnet
+            if(board[y][x] === "m"){
                 ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
-            }
-            //Draw the goal
-            else if(board[y][x] === -1){
+          
+			}
+			
+            //Ziel wird gezeichnet: bisher goldenes X
+            else if(board[y][x] === "z"){ //Code für X-Zeichen folgt
                 ctx.beginPath();
                 ctx.lineWidth = 5;
                 ctx.strokeStyle = "gold";
@@ -58,31 +62,39 @@ function draw(){
             }
         }
     }
-    //Draw the player
+    //Spieler wird gezeichnet
     ctx.beginPath();
-    var half = blockSize/2;
+    var half = blockSize/2; // Kreis wird mittig plaziert
     ctx.fillStyle = "blue";
-    ctx.arc(player.x*blockSize+half, player.y*blockSize+half, half, 0, 2*Math.PI);
+    ctx.arc(player.x*blockSize+half, player.y*blockSize+half, half, 0, 2*Math.PI); //Kreis wird gezeichnet
     ctx.fill();
 }
 
-//Check to see if the new space is inside the board and not a wall
+//Buch wird gezeichnet
+ 
+
+
+//Überprüfung, ob Rand, Mauer oder außerhalb des Spielfelds
+//x und y dürfen nicht kleiner als Null sein (weil Spieler sonst links oben außerhalb vom Spielfeld)
+//x und y dürfen nicht größer als die Länge des Labyrinth/board sein
+//x und y dürfen nicht "m" sein (board[y][x] != "m")
+
+//mit if els neu schreiben
 function canMove(x, y){
-    return (y>=0) && (y<board.length) && (x >= 0) && (x < board[y].length) && (board[y][x] != 1);
+    return (y>=0) && (y<board.length) && (x >= 0) && (x < board[y].length) && (board[y][x] != "m");
 }
 
 $(document).keyup(function(e){
-	//document.getElementById('GameBoardCanvas').onkeyup = function (e) {
-    if((e.which == 38) && canMove(player.x, player.y-1))//Up arrow
-        player.y--;
-    else if((e.which == 40) && canMove(player.x, player.y+1)) // down arrow
-        player.y++;
-    else if((e.which == 37) && canMove(player.x-1, player.y))
-        player.x--;
-    else if((e.which == 39) && canMove(player.x+1, player.y))
-        player.x++;
-    draw();
-    e.preventDefault();
+    if((e.which == 38) && canMove(player.x, player.y-1))//Pfeiltaste nach oben
+        player.y--; //y wird um eins verringert, Spieler bewegt sich nach oben
+    else if((e.which == 40) && canMove(player.x, player.y+1)) //Pfeiltaste nach unten
+        player.y++; // y wird um eins vergrößert, Spieler bewegt sich nach unten
+    else if((e.which == 37) && canMove(player.x-1, player.y)) //Pfeiltaste links
+        player.x--; //x wird um eins verringert, Spieler bewegt sich nach links
+    else if((e.which == 39) && canMove(player.x+1, player.y)) //Pfeiltaste rechts
+        player.x++; //x wird um eins vergrößert, Spieler bewegt sich nach rechts
+    draw(); //nach jedem Tastenanschlag wird das Layrinth neu gezeichnet
+    e.preventDefault(); //übliche Tastenfunktion wird verhindert z. B. scrollen mit Pfeiltasten 
 	});
 
 draw();
