0acbf88633d8599f8d303d8543a939475ed23708
elmade <elena.mahal@gmx.de>
2016-06-21 17:53:12 +0200

    Spielrunde 2
    board 2 wird als 2. Spielrunde geladen

--- program.js
+++ program.js
@@ -3,7 +3,7 @@ var	nochZeit = true;
 if (nochZeit == true){					
 function Bybb(){
 	
-	
+
 	
 	CreateTimer("timer", 25);		//Countdown wird erstellt, Sekundenanzahl festgelegt
 	zielSound = new sound("cheering.mp3");		//Sound beim Erreichen des Ziels, Datei wird verknüpft
@@ -24,10 +24,12 @@ function Bybb(){
 	var buchSound;
 	var zielSoundOhne;
 	
+//mehrere Spielrunden
+	var Spielrunden = new Array (); //Arrays der verschiedenen Spielrunden (im Folgenden einzeln benannt mit board, board2)
+	var aktuelleSpielrunde = 0; //1. Spielrunde entspricht 0
+	var aktuellesBoard; //spricht derzeitiges board der jeweiligen Spielrunde an
 
-	
 canvas = document.getElementById("GameBoardCanvas"); //holt Angaben zu Canvas aus HTML-Datei
-
 
 
 //Labyrinth festlegen: m = Mauer, w = Weg, z = Ziel, b = Buch
@@ -45,6 +47,22 @@ board = [
     [ "m", "w", "w", "w", "w", "w", "w", "w", "m", "z"]
 ];
 
+var board2 = [
+
+	[ "w", "w", "w", "w", "w", "w", "m", "m", "m", "w"],
+    [ "w", "m", "m", "w", "m", "w", "w", "w", "w", "w"],
+    [ "w", "w", "w", "w", "m", "m", "w", "m", "m", "w"],
+    [ "m", "m", "w", "m", "m", "w", "w", "w", "m", "w"],
+    [ "w", "m", "w", "m", "b", "w", "m", "w", "m", "w"],
+    [ "w", "m", "w", "m", "w", "m", "m", "w", "m", "w"],
+    [ "w", "m", "w", "m", "m", "m", "w", "w", "w", "w"],
+    [ "w", "w", "w", "w", "w", "w", "w", "m", "m", "w"],
+    [ "m", "m", "m", "w", "m", "m", "m", "m", "m", "w"],
+    [ "m", "w", "w", "w", "w", "w", "w", "w", "m", "z"]
+];
+
+
+
 //Startposition des Spielers
 player = {
     x: 0,
@@ -52,35 +70,39 @@ player = {
 	book: false //Spieler hat Buch noch nicht berührt
 };
 
+//board wird durch "push" mit board2 ersetzt in 2. Spielrunde
+	Spielrunden.push(board);
+	Spielrunden.push(board2);
 
 //Labyrinth zeichnen
 function draw(){
-	console.log("Karte malen")
+	aktuellesBoard = Spielrunden [aktuelleSpielrunde]; //board mit Gestaltung des Spielfeldes wird je nach Spielrunde anders gestaltet
+	//console.log("Karte malen")
     var width = canvas.width; //Labyrinth passt sich der Breite des Canvas an
     var blockSize = width/board[0].length; //Blockgröße ist abhängig von board
     var ctx = canvas.getContext('2d');
     ctx.clearRect(0, 0, width, width);
     ctx.fillStyle="maroon"; //Farbe für Mauer
 	
-	
 
 
-    //Schleife durch Array für das Zeichnen der Wände und des Ziels
+ //Schleife durch Array für das Zeichnen der Wände und des Ziels
     for(var y = 0; y < board.length; y++){ //Schleife beginnt links oben im Spielfeld, läuft durch Array, Endposition ist unten rechts im Spielfeld
         for(var x = 0; x < board[y].length; x++){
             //Mauer wird gezeichnet
-            if(board[y][x] === "m"){
+            if(aktuellesBoard[y][x] === "m"){ //da Gestaltung des Spielfeldes je nach Spielrunde werden board und board2 nicht extra angesprochen, 
+												//sonden über die gemeinsame Variabel "aktuellesBoard"
 				ctx.fillStyle = "maroon"
                 ctx.fillRect(x*blockSize, y*blockSize, blockSize, blockSize);
             }
 			//Buch mit Bild
-			else if(board[y][x] === "b"){ 
+			else if(aktuellesBoard[y][x] === "b"){ 
 			ctx.drawImage(document.getElementById('buch'), 250, 250, 50, 50)   //X- und Y-Koordinaten, sowie Angaben zur Bildgröße: Breite, Höhe
 			}
 		
 			
             //Ziel mit Bild
-            else if(board[y][x] === "z"){
+            else if(aktuellesBoard[y][x] === "z"){
                 ctx.drawImage(document.getElementById('library'), 530, 530, 75, 75);
 				 };
         };
@@ -105,8 +127,10 @@ function draw(){
 		else if (y<0){return false;} //y darf nicht kleiner als Null sein (weil Spieler sonst links oben außerhalb vom Spielfeld)
 		else if (x>=board.length){return false;} //x darf nicht größer als die Länge des Labyrinth/board sein
 		else if (y>=board.length){return false;} //y darf nicht größer als die Länge des Labyrinth/board sein
-		else if (board[y][x] == "m"){return false;} // x und y dürfen nicht "m" (Mauer) sein
+		else if (aktuellesBoard[y][x] == "m"){return false;} // x und y dürfen nicht "m" (Mauer) sein
 		//else if (board[player.y][player.x] =="z" && player.book=true {return false;}
+		else if (aktuellesBoard[y][x] == "z"){alert("Du hast es geschafft! Das Buch ist rechtzeitig in der Bibliothek.");
+			aktuelleSpielrunde++; player.x=0; player.y=0;clearTimeout;} //Ziel wird erkannt und neue Spielrunde geladen
 		else {return true;}	
 	};
 
@@ -145,7 +169,7 @@ function Ziel() {
 		//Spieler hat Buch eingesammelt
 		if (player.book == true){
 		zielSound.play();
-		alert("Du hast es geschafft! Das Buch ist rechtzeitig in der Bibliothek.");
+		//alert("Du hast es geschafft! Das Buch ist rechtzeitig in der Bibliothek.");
 		nochZeit = false;
 		console.log(nochZeit);
 		}
@@ -164,6 +188,8 @@ function Ziel() {
 	};
 //return;
 };
+
+
 
 //Sound-Objekt, legt Eigenschaften der Sounds fest und ermöglicht dem Programm mit Sounds umzugehen
 function sound(src) {
